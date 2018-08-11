1912f4cdded106904f6edbe1f4db4475d224cfd0
George Pantazis <gcpantazis@gmail.com>
2018-08-11 13:43:40 -0700

    Board layout, with simple exception if collisions invalidate a board

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,30 @@
 import React from 'react';
+import { each, find, map } from 'lodash';
+
+function hasShip({
+  ships, board, rowIdx, colIdx,
+}) {
+  let match = false;
+
+  each(board, (shipData, shipName) => {
+    const shipDefinition = find(ships, { id: shipName });
+
+    for (let i = 0; i < shipDefinition.length; i += 1) {
+      if (
+        shipData.x + (shipData.direction === 'horizontal' ? i : 0) === colIdx &&
+        shipData.y + (shipData.direction === 'vertical' ? i : 0) === rowIdx
+      ) {
+        if (match === true) {
+          throw new Error('Invalid board! Two ships overlapping.');
+        }
+
+        match = true;
+      }
+    }
+  });
+
+  return match;
+}
 
 export default class extends React.Component {
   static async getInitialProps() {
@@ -8,10 +34,90 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      ships: [
+        {
+          id: 'battleship',
+          length: 5,
+        },
+        {
+          id: 'destroyer',
+          length: 4,
+        },
+      ],
+      boardWidth: 15,
+      boardHeight: 15,
+      boards: {
+        playerOne: {
+          battleship: {
+            x: 1,
+            y: 3,
+            direction: 'horizontal',
+          },
+          destroyer: {
+            x: 6,
+            y: 6,
+            direction: 'vertical',
+          },
+        },
+        playerTwo: {
+          battleship: {
+            x: 2,
+            y: 3,
+            direction: 'horizontal',
+          },
+          destroyer: {
+            x: 6,
+            y: 5,
+            direction: 'vertical',
+          },
+        },
+      },
+      moves: [],
+    };
   }
 
   render() {
-    return <div>Hello</div>;
+    const {
+      ships, boardWidth, boardHeight, boards, moves,
+    } = this.state;
+
+    return (
+      <div>
+        {map(boards, (board, playerId) => (
+          <div key={`${playerId}`}>
+            {playerId}
+            {map([...new Array(boardHeight)], (b, rowIdx) => (
+              <div key={`${playerId}-row${rowIdx}`}>
+                {map([...new Array(boardWidth)], (c, colIdx) => (
+                  <span
+                    key={`${playerId}-row${rowIdx}-col${colIdx}`}
+                    hasShip={hasShip({
+                      ships,
+                      board,
+                      rowIdx,
+                      colIdx,
+                    })}
+                    style={{
+                      background: hasShip({
+                        ships,
+                        board,
+                        rowIdx,
+                        colIdx,
+                      })
+                        ? 'red'
+                        : 'blue',
+                    }}
+                  >
+                    <input type="checkbox" />
+                  </span>
+                ))}
+              </div>
+            ))}
+            <br />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
