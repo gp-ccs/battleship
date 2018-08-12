476cb46a6d1d56cbca748145af67ebcb93d8f8a7
George Pantazis <gcpantazis@gmail.com>
2018-08-12 11:07:09 -0700

    Add game logic for the placement of ships
    - Also creating bare-bones UI for the game, following traditional
    battleship (enemy on top, your ships below)
    - Time permitting I'll add some sort of "switch players" treatment

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,158 @@
 import React from 'react';
-import { map } from 'lodash';
+import { find, map } from 'lodash';
 
 import { wrapPageInRedux, Actions } from '../lib/store';
 import { hasShip } from '../lib/utils';
+
+const Board = ({
+  height,
+  width,
+  playerId,
+  boardDefinition,
+  shipDefinitons,
+  showShips,
+  onClickGrid,
+}) => (
+  <div>
+    {map([...new Array(height)], (b, rowIdx) => (
+      <div key={`${playerId}-row${rowIdx}`}>
+        {map([...new Array(width)], (c, colIdx) => (
+          <span
+            key={`${playerId}-row${rowIdx}-col${colIdx}`}
+            style={{
+              background:
+                showShips &&
+                hasShip({
+                  ships: shipDefinitons,
+                  board: boardDefinition,
+                  rowIdx,
+                  colIdx,
+                })
+                  ? 'lightgray'
+                  : 'blue',
+            }}
+          >
+            <input
+              type="checkbox"
+              onClick={(e) => {
+                e.preventDefault();
+                onClickGrid({
+                  x: colIdx,
+                  y: rowIdx,
+                });
+              }}
+            />
+          </span>
+        ))}
+      </div>
+    ))}
+  </div>
+);
 
 class BattleshipPage extends React.Component {
   static async getInitialProps() {
     return {};
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      placementDirection: 'horizontal',
+    };
+  }
+
   render() {
     const {
-      ships, boardWidth, boardHeight, boards, moves, activePlayerId,
+      ships, boardWidth, boardHeight, boards, activePlayerId, placeShip,
     } = this.props;
+
+    const { placementDirection } = this.state;
+    const activePlayerBoard = boards[activePlayerId];
+
+    const adversaryId = find(Object.keys(boards), o => o !== activePlayerId);
+    const adversaryBoard = boards[adversaryId];
+
+    const shipToBePlaced = find(ships, o => !find(activePlayerBoard, { shipId: o.id }));
 
     return (
       <div>
-        {map(boards, (board, playerId) => (
-          <div key={`${playerId}`}>
-            {playerId}
-            {map([...new Array(boardHeight)], (b, rowIdx) => (
-              <div key={`${playerId}-row${rowIdx}`}>
-                {map([...new Array(boardWidth)], (c, colIdx) => (
-                  <span
-                    key={`${playerId}-row${rowIdx}-col${colIdx}`}
-                    hasShip={hasShip({
-                      ships,
-                      board,
-                      rowIdx,
-                      colIdx,
-                    })}
-                    style={{
-                      background: hasShip({
-                        ships,
-                        board,
-                        rowIdx,
-                        colIdx,
-                      })
-                        ? 'lightgray'
-                        : 'blue',
-                    }}
-                  >
-                    <input type="checkbox" disabled={playerId !== activePlayerId} />
-                  </span>
-                ))}
-              </div>
-            ))}
-            <br />
+        {activePlayerId}
+        {shipToBePlaced && (
+          <div>
+            Place your {shipToBePlaced.id}
+            <input
+              type="radio"
+              value="horizontal"
+              checked={placementDirection === 'horizontal'}
+              onChange={(e) => {
+                this.setState({
+                  placementDirection: e.target.value,
+                });
+              }}
+            />{' '}
+            Horizontal
+            <input
+              type="radio"
+              value="vertical"
+              checked={placementDirection === 'vertical'}
+              onChange={(e) => {
+                this.setState({
+                  placementDirection: e.target.value,
+                });
+              }}
+            />Vertical
           </div>
-        ))}
+        )}
+        <div>ENEMY</div>
+        <Board
+          height={boardHeight}
+          width={boardWidth}
+          playerId={adversaryId}
+          boardDefinition={adversaryBoard}
+          shipDefinitons={ships}
+          showShips={false}
+          onClickGrid={({ x, y }) => {}}
+        />
+        <div>YOUR FLEET</div>
+        <Board
+          height={boardHeight}
+          width={boardWidth}
+          playerId={activePlayerId}
+          boardDefinition={activePlayerBoard}
+          shipDefinitons={ships}
+          showShips
+          onClickGrid={({ x, y }) => {
+            if (activePlayerBoard.length < ships.length) {
+              placeShip({
+                playerId: activePlayerId,
+                shipId: shipToBePlaced.id,
+                x,
+                y,
+                direction: placementDirection,
+              });
+            }
+          }}
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = ({
-  ships, boardWidth, boardHeight, boards, moves, activePlayerId,
+  ships, boardWidth, boardHeight, boards, activePlayerId,
 }) => ({
   ships,
   boardWidth,
   boardHeight,
   boards,
-  moves,
   activePlayerId,
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  placeShip: (options) => {
+    dispatch(Actions.placeShip(options));
+  },
+});
 
 export default wrapPageInRedux(BattleshipPage, mapStateToProps, mapDispatchToProps);
